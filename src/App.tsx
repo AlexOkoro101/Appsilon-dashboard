274d5d9ed49c6d4adf4ef4b9c2ae04d4b5afe7dc
unknown <alexokoro342@gmail.com>
2022-06-01 15:06:07 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import {
   ChakraProvider,
   theme
 } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import AdminLayout from './layouts/Admin';
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminLayout />}></Route>
+        <Route path="/"element={<Navigate to="/admin/dashboard" replace />} />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 );
